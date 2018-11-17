5f911fcfe9e75568644c8a0b81d6985e3b235036
quidsup <ooop@btinternet.com>
2018-11-17 18:28:59 +0000

    fixed formatting

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,34 +6,37 @@
 
 #TODO Delete MariaDB Tables
 
-#User Configerable variables-----------------------------------------
-readonly FOLDER_SBIN="/usr/local/sbin"
-readonly FOLDER_ETC="/etc"
-
-#Program Settings----------------------------------------------------
-INSTALL_LOCATION="${HOME}/notrack"
-
+#######################################
+# Constants
+#######################################
+readonly FOLDER_SBIN="/usr/local/sbin"                     #User configurable
+readonly FOLDER_ETC="/etc"                                 #User configurable
 
 #######################################
+# Global Variables
+#######################################
+INSTALL_LOCATION="$HOME/notrack"
+
+
+#--------------------------------------------------------------------
 # Stop service
-#    with either systemd or sysvinit or runit
-#
+#   with either systemd or sysvinit or runit
 # Globals:
 #   None
 # Arguments:
-#   None
+#   $1. Service name
 # Returns:
 #   None
-#######################################
+#--------------------------------------------------------------------
 service_stop() {
   if [[ -n $1 ]]; then
     echo "Stopping $1"
     if [ "$(command -v systemctl)" ]; then     #systemd
-      sudo systemctl stop $1
+      sudo systemctl stop "$1"
     elif [ "$(command -v service)" ]; then     #sysvinit
-      sudo service $1 stop
+      sudo service "$1" stop
     elif [ "$(command -v sv)" ]; then          #runit
-      sudo sv down $1
+      sudo sv down "$1"
     else
       echo "Unable to stop services. Unknown service supervisor"
       exit 21
@@ -42,55 +45,98 @@ service_stop() {
 }
 
 
-#Copy File-----------------------------------------------------------
-CopyFile() {
-  #$1 Source
-  #$2 Target
-  if [ -e "$1" ]; then
+#--------------------------------------------------------------------
+# Copy File
+#   Copies source file to destination if it exists
+# Globals:
+#   None
+# Arguments:
+#   $1: Source
+#   $2: Target
+# Returns:
+#   None
+#--------------------------------------------------------------------
+copyfile() {
+  if [ -e "$1" ]; then                                     #Check file exists
     echo "Copying $1 to $2"
     cp "$1" "$2"
   else
     echo "File $1 not found"
   fi
 }
-#Delete old file if it Exists----------------------------------------
-DeleteFile() {
-  if [ -e "$1" ]; then
+
+
+#--------------------------------------------------------------------
+# Delete File
+#   Deletes a file if it exists
+# Globals:
+#   None
+# Arguments:
+#   $1: Source
+# Returns:
+#   None
+#--------------------------------------------------------------------
+deletefile() {
+  if [ -e "$1" ]; then                                     #Check file exists
     echo "Deleting file $1"
-    rm "$1"    
+    rm "$1"
   fi
 }
-#Delete old file if it Exists----------------------------------------
-DeleteFolder() {
-  if [ -d "$1" ]; then
+
+
+
+#--------------------------------------------------------------------
+# Delete Folder
+#   Deletes a folder if it exists
+# Globals:
+#   None
+# Arguments:
+#   $1: Source
+# Returns:
+#   None
+#--------------------------------------------------------------------
+deletefolder() {
+  if [ -d "$1" ]; then                                     #Check folder exists
     echo "Deleting folder $1"
-    rm -rf "$1"    
+    rm -rf "$1"
   fi
 }
-#Find NoTrack--------------------------------------------------------
-Find_NoTrack() {
-  #This function finds where NoTrack is installed
-  #1. Check current folder
-  #2. Check users home folders
-  #3. Check /opt/notrack
-  #4. If not found then abort
-  
-  if [ -e "$(pwd)/notrack.sh" ]; then
-    INSTALL_LOCATION="$(pwd)"  
+
+
+#--------------------------------------------------------------------
+# Find NoTrack
+#   This function finds where NoTrack is installed
+#   1. Check current folder
+#   2. Check users home folders
+#   3. Check /opt/notrack
+#   4. If not found then abort
+#
+# Globals:
+#   INSTALL_LOCATION
+# Arguments:
+#   None
+# Returns:
+#   1 if found
+#--------------------------------------------------------------------
+find_notrack() {
+  local homefolders=""
+
+  if [ -e "$(pwd)/notrack.sh" ]; then                      #Check current folder
+    INSTALL_LOCATION="$(pwd)"
     return 1
   fi
-  
-  for HomeDir in /home/*; do
-    if [ -d "$HomeDir/NoTrack" ]; then 
-      INSTALL_LOCATION="$HomeDir/NoTrack"
+
+  for homefolders in /home/*; do                           #Check all folders under /home
+    if [ -d "$homefolders/NoTrack" ]; then 
+      INSTALL_LOCATION="$homefolders/NoTrack"
       break
-    elif [ -d "$HomeDir/notrack" ]; then 
-      INSTALL_LOCATION="$HomeDir/notrack"
+    elif [ -d "$homefolders/notrack" ]; then 
+      INSTALL_LOCATION="$homefolders/notrack"
       break
     fi
   done
 
-  if [[ $INSTALL_LOCATION == "" ]]; then
+  if [[ $INSTALL_LOCATION == "" ]]; then                   #Check /opt
     if [ -d "/opt/notrack" ]; then
       INSTALL_LOCATION="/opt/notrack"
     else
@@ -100,17 +146,17 @@ Find_NoTrack() {
       exit 22
     fi
   fi
-  
+
   return 1
 }
 
-#Main----------------------------------------------------------------
 
-Find_NoTrack                                     #Where is NoTrack located?
+#Main----------------------------------------------------------------
+find_notrack                                               #Where is NoTrack located?
 
 if [[ "$(id -u)" != "0" ]]; then
   echo "Root access is required to carry out uninstall of NoTrack"
-  echo "sudo bash uninstall.sh"
+  echo "Usage: sudo bash uninstall.sh"
   exit 5
   #su -c "$0" "$@" - This could be an alternative for systems without sudo
 fi
@@ -132,50 +178,50 @@ echo
 
 echo "Deleting Symlinks for Web Folders"
 echo "Deleting Sink symlink"
-DeleteFile "/var/www/html/sink"
+deletefile "/var/www/html/sink"
 echo "Deleting Admin symlink"
-DeleteFile "/var/www/html/admin"
+deletefile "/var/www/html/admin"
 echo
 
 echo "Restoring Configuration files"
 echo "Restoring Dnsmasq config"
-CopyFile "/etc/dnsmasq.conf.old" "/etc/dnsmasq.conf"
+copyfile "/etc/dnsmasq.conf.old" "/etc/dnsmasq.conf"
 echo "Restoring Lighttpd config"
-CopyFile "/etc/lighttpd/lighttpd.conf.old" "/etc/lighttpd/lighttpd.conf"
+copyfile "/etc/lighttpd/lighttpd.conf.old" "/etc/lighttpd/lighttpd.conf"
 echo "Removing Local Hosts file"
-DeleteFile "/etc/localhosts.list"
+deletefile "/etc/localhosts.list"
 echo
 
 echo "Removing Log file rotator"
-DeleteFile "/etc/logrotate.d/notrack"
+deletefile "/etc/logrotate.d/notrack"
 echo
 
 echo "Removing Cron job"
-DeleteFile "/etc/cron.d/ntrk-parse"
+deletefile "/etc/cron.d/ntrk-parse"
 echo
 
 echo "Deleting NoTrack scripts"
 echo "Deleting dns-log-archive"
-DeleteFile "$FOLDER_SBIN/dns-log-archive"
+deletefile "$FOLDER_SBIN/dns-log-archive"
 echo "Deleting notrack"
-DeleteFile "$FOLDER_SBIN/notrack"
+deletefile "$FOLDER_SBIN/notrack"
 echo "Deleting ntrk-exec"
-DeleteFile "$FOLDER_SBIN/ntrk-exec"
+deletefile "$FOLDER_SBIN/ntrk-exec"
 echo "Deleting ntrk-pause"
-DeleteFile "$FOLDER_SBIN/ntrk-pause"
+deletefile "$FOLDER_SBIN/ntrk-pause"
 echo "Deleting ntrk-parser"
-DeleteFile "$FOLDER_SBIN/ntrk-parser"
+deletefile "$FOLDER_SBIN/ntrk-parser"
 echo
 
 echo "Removing root permissions for www-data to launch ntrk-exec"
 sed -i '/www-data/d' /etc/sudoers
 
 echo "Deleting /etc/notrack Folder"
-DeleteFolder "$FOLDER_ETC/notrack"
+deletefolder "$FOLDER_ETC/notrack"
 echo 
 
 echo "Deleting Install Folder"
-DeleteFolder "$INSTALL_LOCATION"
+deletefolder "$INSTALL_LOCATION"
 echo
 
 echo "Finished deleting all files"
