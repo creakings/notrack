942bf95d4aec3c0be4f9d569e25ea635c812c24b
quidsup <ooop@btinternet.com>
2020-04-20 16:25:17 +0100

    initial release

--- sink/index.php
+++ sink/index.php
@@ -5,7 +5,7 @@ $paramlist = array();                                      //Sorted list of GET 
 /********************************************************************
  *  Extract URLs
  *    Check for any URLs
- *    Strip tags from GET values and store them in $paramlist
+ *    Strip tags from GET keys and values then store them in $paramlist
  *
  *  Params:
  *    None
@@ -20,7 +20,7 @@ function extract_urls() {
     if ($key == 'ntrkorigin') continue;                    //Skip ntrkorigin
 
     $taglessvalue = strip_tags($value);                    //Prevent XSS
-    $paramlist[$key] = $taglessvalue;                      //Store sorted parameter
+    $paramlist[strip_tags($key)] = $taglessvalue;          //Store parameter
 
     //Is the taglessvalue a URL?
     if (preg_match('/^https?:\/\/[\w-._]{3,256}(\/[\w-_.%\/]*)?(\?[\w%&=.:#@~$]*)?$/', $taglessvalue)) {
@@ -47,10 +47,12 @@ function draw_urllist() {
     return;
   }
 
-  echo 'Extracted URL&rsquo;s:<br>'.PHP_EOL;
+  echo '<h4>Extracted URL&rsquo;s:</h4>'.PHP_EOL;
+  echo '<ul>'.PHP_EOL;
   foreach($urllist as $url) {
-    echo "<a href=\"{$url}\">{$url}</a><br>".PHP_EOL;
+    echo "<li><a href=\"{$url}\">{$url}</a></li>".PHP_EOL;
   }
+  echo '</ul>'.PHP_EOL;
 }
 
 
@@ -66,7 +68,7 @@ function draw_paramlist() {
   global $paramlist;
   $i = 1;                                                  //Beautify
 
-  echo 'Parameter list:<br>'.PHP_EOL;
+  echo '<h4>Parameter list:</h4>'.PHP_EOL;
 
   if (count($paramlist) == 0) {                            //Any parameters extracted?
     echo 'None specified<br>'.PHP_EOL;
@@ -75,24 +77,40 @@ function draw_paramlist() {
 
   ksort($paramlist);
 
+  echo '<table>'.PHP_EOL;
   foreach($paramlist as $key => $value) {
-    echo "{$i}: {$key} - {$value}<br>".PHP_EOL;
+    echo "<tr><td>{$i}</td><td>{$key}</td><td>{$value}</td></tr>".PHP_EOL;
     $i++;
   }
+  echo '</table>'.PHP_EOL;
 }
 $host = $_SERVER['HTTP_HOST'];
 $ntrkorigin = $_GET['ntrkorigin'] ?? '';
 
-echo '<!DOCTYPE html>'.PHP_EOL;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link href="./sink.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=0.9">
+  <title>Blocked by NoTrack</title>
+</head>
+<body>
+<div class="circle1"></div>
+<div class="circle2"></div>
+<div class="sys-group">
+
+<?php
 echo '<h1>Blocked by NoTrack</h1>'.PHP_EOL;
-echo '<h3>Sorry about that</h3>'.PHP_EOL;
-echo "Host: $host<br>";
-echo "Folder: $ntrkorigin<br>";
-echo '<br>'.PHP_EOL;
+echo '<h2>Sorry about that</h2>'.PHP_EOL;
+echo "<h3><a href=\"https://{$host}${ntrkorigin}\">{$host}${ntrkorigin}</a></h3>".PHP_EOL;
+
 extract_urls();
 draw_urllist();
-echo '<br>'.PHP_EOL;
 draw_paramlist();
-echo '<br>'.PHP_EOL;
-echo '</html>'.PHP_EOL;
+
 ?>
+</div>
+</body>
+</html>
