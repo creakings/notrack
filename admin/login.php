3623623ff5ff3e692bfb20158247f2c15ae9251a
Quids <ooop@btinternet.com>
2020-09-29 22:38:33 +0100

    completed rewrite

--- admin/login.php
+++ admin/login.php
@@ -1,32 +1,28 @@
 <?php
-//Title : Login
-//Description : Controls loin for NoTrack, validating username and password, and throttling password attemtps
-//Author : QuidsUp
-//Date : 2015-03-25
-
-
-//1. Start Session
-//2. Check if session is already active then return to index.php
-//3. Check if password is required
-//3a. If not return to index.php (Otherwise you get trapped on this page)
-//4. Has a password been sent with HTTP POST?
-//4a. Check if delay is imposed on Memcache variable 'Delay'
-//4ai. If yes then set $message to wait and don't evaluate logon attempt, jump to 5.
-//4b. Username is optional, check if it has been set in HTTP POST, otherwise set it to blank
-//4c. Create access log file if it doesn't exist
-//4d. Use PHP password_verify function to check hashed version of user input with hash in $config->password
-//4ei. If username and password match set SESSION['sid'] to 1 (Future version may use a random number, to make it even harder to hijack a session)
-//4eii. On failure write Delay into Memcache and show message of Incorrect Username or Password
-//      Add entry into ntrk-access.log to allow functionality with Fail2ban
-//      (Deny attacker knowledge of whether Username OR Password is wrong)
-
-//5. Draw basic top menu
-//6. Draw form login
-//7. Draw box with $message (If its set)
-//8. Draw hidden box informing user that Cookies must be enabled
-//9. Use Javascript to check if Cookies have been enabled
-//9a. If Cookies are disabled then set 8. to Visible
-
+/********************************************************************
+ *  Title : Login
+ *  Description : Controls login for NoTrack, validating username and password,
+ *                also throttles attemtps based on IP or username
+ *  Author : QuidsUp
+ *  Date : 2015-03-25, rewrite 2020-09-27
+ *
+ *  1. Start Session
+ *  2. Check if session is already active then return to index.php
+ *  3. Check if password is required
+ *  3a. If not return to index.php (Otherwise you get trapped on this page)
+ *  4. Has a password been sent with HTTP POST?
+ *  4a. Check if delay is imposed in IP or username
+ *      If yes then set $message to wait and don't evaluate logon attempt, jump to 5.
+ *  4b. Use PHP password_verify function to check hashed version of user input with hash in $config->password
+ *  4c. On failure log Delay on Memcache showing time and attempts.
+ *      Show message of Incorrect Username or Password
+ *      Add entry into error.log to allow functionality with Fail2ban
+ *  5. Draw login form
+ *  6. Draw box with $message (If its set)
+ *  7. Draw hidden box informing user that Cookies must be enabled
+ *  8. Use Javascript to check if Cookies have been enabled
+ *     If Cookies are disabled then set 7. to Visible
+ */
 require('./include/global-vars.php');
 require('./include/global-functions.php');
 require('./include/config.php');
@@ -38,7 +34,8 @@ $username = '';
 /********************************************************************
  *  Check IP Delay
  *    Check Memcache to see if a delay should be imposed against visitor IP
- *    Delay is last recorded time + no. attemps squared
+ *    Delay is last recorded time + no. attemps ^ 1.5
+ *    This is lower as user may have accidentally typed wrong username
  *
  *  Params:
  *    Username (str): Username value to retrieve from Memcache
@@ -61,13 +58,12 @@ function check_ip_delay($userip) {
     return false;
   }
 
-  $calcdelay = ($time_userip + ($attempts_userip ** 2));
-  $difference = $calcdelay - time();
-  if (time() > $calcdelay) {                               //Has delay passed?
+  //Has delay passed?
+  if (time() > ($time_userip + ($attempts_userip ** 1.5))) {
     return false;
   }
-  echo "IP Fail {$attempts_userip}, Delay {$difference}";
-  return true;
+
+  return true;                                             //At this point impose a delay
 }
 
 
@@ -97,20 +93,19 @@ function check_username_delay($username) {
     return false;
   }
 
-  $calcdelay = ($time_username + ($attempts_username ** 2));
-  $difference = $calcdelay - time();
-  if (time() > $calcdelay) {                               //Has delay passed?
+  //Has delay passed?
+  if (time() > ($time_username + ($attempts_username ** 2))) {
     return false;
   }
-  echo "User Fail {$attempts_username}, Delay {$difference}";
-  return true;
+
+  return true;                                             //At this point impose a delay
 }
 
 
 /********************************************************************
  *  Set Delay against visitor IP
  *    Record current time and increase attempts
- *    Hold values for 900 seconds (15 mins)
+ *    Hold values for 600 seconds (10 mins)
  *  Params:
  *    UserIP (str): IP value to Set to from Memcache
  *  Return:
@@ -118,25 +113,24 @@ function check_username_delay($username) {
  */
 function delay_ip($userip) {
   global $mem;
+  $memkey = '';                                            //Memcache name for attempts
 
-  if ($userip == '') {
-    return;
-  }
+  if ($userip == '') return;                               //Not dealing with blank vals
 
   $memkey = "{$userip}_attempts";
 
   if ($mem->get($userip)) {                                //Is userip value known?
-    $mem->replace($userip, time(), 0, 900);                //Yes - Replace old time value
+    $mem->replace($userip, time(), 0, 600);                //Yes - Replace old time value
   }
   else {
-    $mem->set($userip, time(), 0, 900);                    //No - Set a new time value
+    $mem->set($userip, time(), 0, 600);                    //No - Set a new time value
   }
 
   if ($mem->get($memkey)) {                                //Is user attempts value known?
     $mem->increment($memkey, 1);                           //Yes - Increase by 1
   }
   else {
-    $mem->set($memkey, 1, 0, 900);
+    $mem->set($memkey, 1, 0, 600);
   }
 }
 
@@ -152,10 +146,9 @@ function delay_ip($userip) {
  */
 function delay_username($username) {
   global $mem;
+  $memkey = '';                                            //Memcache name for attempts
 
-  if ($username == '') {
-    return;
-  }
+  if ($username == '') return;                             //Not dealing with blank vals
 
   $memkey = "{$username}_attempts";
 
@@ -175,79 +168,75 @@ function delay_username($username) {
 }
 /********************************************************************
  *  Validate Password
- *    Populates filter bar with Mark resolved
+ *    1. Check if delay should be imposed on IP
+ *    2. Check if delay should be imposed on username
+ *    3. Verify password
+ *       Activate session and exit this page if correct
+ *    4. Increase delay on IP and username if password is wrong
  *  Params:
- *    None
+ *    Username, Password, IP
  *  Return:
- *    None
+ *    message for error-box
  */
 function validate_password($username, $password, $userip) {
   global $config;
   $message = '';
 
   if (check_ip_delay($userip)) {
-    $message = 'Unable to verify';
+    $message = 'Please Wait';
     return $message;
   }
 
   if (check_username_delay($username)) {
-    $message = 'Unable to verify';
+    $message = 'Please Wait';
     return $message;
   }
 
-  if (($username == $config->username) && (password_verify($password, $config->password))) {
-    activate_session();                      //Set session to enabled
-    header('Location: ./index.php');         //Redirect to index.php
+  //Use built in password_verify function to compare with $config->password hash
+  if (($username == $config->username) and (password_verify($password, $config->password))) {
+    activate_session();                                    //Set session to enabled
+    header('Location: ./index.php');                       //Redirect to index.php
     exit;
   }
+  else {
+    if ($username != '') {
+      //Output attempt to error.log
+      trigger_error("Failed login from {$userip} with username {$username}", E_USER_NOTICE);
+    }
 
-  $message = "Incorrect username or password";   //Deny attacker knowledge of whether username OR password is wrong
+    //Deny attacker knowledge of whether username OR password is wrong
+    $message = 'Incorrect username or password';
+  }
 
   delay_ip($userip);
   delay_username($username);
 
   return $message;
 }
+/********************************************************************/
 
-if (! $config->is_password_protection_enabled()) {         //Leave if password protection disabled
+//Leave if password protection is disabled
+if (! $config->is_password_protection_enabled()) {
   header('Location: ./index.php');
   exit;
 }
 
 session_start();
+//Leave is session is already active
 if (is_active_session()) {
   header('Location: ./index.php');
   exit;
 }
 
-
+//Has a password been entered?
 if (isset($_POST['password'])) {
   $username = $_POST['username'] ?? '';
   $password = $_POST['password'];
   $userip = $_SERVER['REMOTE_ADDR'];
 
-  $username = strip_tags($username);
-
+  $username = strip_tags($username);                       //Reduce risk of XSS
+  //Validate password and impose any delays
   $message = validate_password($username, $password, $userip);
-  /*if ($mem->get('delay')) {                      //Load Delay from Memcache
-    $message = 'Wait';                           //If it is set then Wait
-  }
-  else {                                         //No Delay, check Password
-    $password = $_POST['password'];
-
-
-    //Use built in password_verify function to compare with $config->password hash
-
-
-
-
-    //At this point the Password is Wrong
-    $mem->set('delay', 10, 0, 10);
-    $message = "Incorrect username or password";   //Deny attacker knowledge of whether username OR password is wrong
-
-    //Output attempt to error.log
-    trigger_error("Failed login from {$_SERVER['REMOTE_ADDR']} with username {$username}", E_USER_NOTICE);
-  }*/
 }
 ?>
 <!DOCTYPE html>
@@ -262,7 +251,7 @@ if (isset($_POST['password'])) {
 
 <body>
 <?php
-if ($message != '') {                            //Any Message to show?
+if ($message != '') {                                      //Any Message to show?
   echo '<div id="error-box">'.PHP_EOL;
   echo '<h4>'.$message.'</h4>'.PHP_EOL;
   echo '</div>'.PHP_EOL;
@@ -293,7 +282,8 @@ echo '</div>'.PHP_EOL;
 ?>
 
 <script>
-if (! navigator.cookieEnabled) {                           //has user disabled cookies for this site?
+//has user disabled cookies for this site?
+if (! navigator.cookieEnabled) {
   document.getElementById("cookie-box").style.display = "block";
   document.getElementById("fade").style.display = "block";
 }
